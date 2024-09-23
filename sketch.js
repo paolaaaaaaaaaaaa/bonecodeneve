1f8f3d54daf902e1bb6b9fb6ebfb25d9da113785
paolaaaaaaaaaaaa <paola.monteiro.silva@escola.pr.gov.br>
2024-09-23 10:08:18 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  fill("white")
+  ellipse(200, 300, 250, 230) 
+   ellipse(200, 170, 150, 130)
+  ellipse(200, 80, 100, 90)
+  fill("blue")
+  circle(180, 60, 15)
+  circle(220, 60, 15)
+  fill ("orange")
+  triangle(200, 70, 200, 90, 230, 75)
+  line(190, 100, 210, 100)
+  fill ("brown")
+  circle(199, 140, 15)
+  circle(199, 170, 15)
+  circle(199, 200, 15)
+  circle(199, 260, 15)
+  circle(199, 290, 15)
+  circle(199, 320, 15)
+  circle(199, 350, 15)
+  circle(199, 380, 15)
+  line (130, 170, 100, 100)
+line (270, 170, 300, 100)
+  stroke(orange)
+}
